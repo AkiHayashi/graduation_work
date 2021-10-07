6f1757bf44cd9e2999fd8334163dad873745a553
aki hayashi <hayashi1019toy@gmail.com>
2021-10-07 13:11:06 +0900

    feat: 🎸 add guest login function
    edit user model to create admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :authentication_keys => [:login_name]
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.login_name = "ゲスト"
+    end
+  end
+  def self.admin_guest
+    find_or_create_by!(email: 'admin_guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.admin = true
+      user.login_name = "ゲスト管理者"
+    end
+  end
 end
