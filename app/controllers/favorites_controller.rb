e7f7858b32420dbd4ea2a736413a04e69b5be930
aki hayashi <hayashi1019toy@gmail.com>
2021-10-18 22:41:18 +0900

    feat: 🎸 edit favorite function with Ajax
    ✅ Closes: #46

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    @diary = Diary.find(params[:diary])
-    current_user.favorite(@diary)
+    @diary = Diary.find(params[:id])
+    favorite = current_user.favorites.new(diary_id: @diary.id)
+    favorite.save
   end
 
   def destroy
-    @diary = Favorite.find(params[:id]).diary
-    current_user.unfavorite(@diary)
+    @diary = Diary.find(params[:id])
+    favorite = current_user.favorites.find_by(diary_id: @diary.id)
+    favorite.destroy
   end
 end
